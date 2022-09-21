a3d659d6e6c2a8d8c43a59811e4e53d7e85aeb10
putracode <saputra219021@gmail.com>
2022-09-21 12:04:27 +0700

    Dashboard

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -4,14 +4,25 @@ namespace App\Http\Controllers\Backend;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Booking;
+use App\Models\PublicFacility;
+use App\Models\Room;
+use App\Models\RoomFacility;
 
 class DashboardController extends Controller
 {
     public function indexAdmin(){
-        return view('admin.dashboard');
+        return view('admin.dashboard',[
+            'rooms' => Room::count(),
+            'rooms_facilities' => RoomFacility::count(),
+            'facilities' => PublicFacility::count(),
+            'booking' => Booking::count(),
+        ]);
     }
 
     public function indexReceptionist(){
-        return view('receptionist.dashboard');
+        return view('receptionist.dashboard',[
+            'booking' => Booking::count()
+        ]);
     }
 }
--- resources/views/receptionist/dashboard.blade.php
+++ resources/views/receptionist/dashboard.blade.php
@@ -1,8 +1,16 @@
 @extends('layout.main')
 
 @section('content')
-<div class="page-title">
+{{-- <div class="page-title">
     <h3>Dashboard</h3>
     <p class="text-subtitle text-muted">A good dashboard to display your statistics</p>
+</div> --}}
+<div class="col-md-3">
+    <div class="card text-white bg-primary">
+        <div class="card-body">
+            <h1>{{ $booking }}</h1>
+            <a href="/receptionist/booking" class="text-white">Booking</a>
+        </div>
+    </div>
 </div>
 @endsection
