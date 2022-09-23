7e9dbb25feecab3eb90ea2c12dab9172c5cf0d9d
RendiPurwito <rendipurwito@gmail.com>
2022-09-23 19:55:03 +0700

    Perbaikan Button Create

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -65,7 +65,7 @@
 <div class="card">
     <div class="card-header">
         Room Facility Data
-        <a href="/admin/room-facility" class="btn btn-primary btn-sm float-right ml-3">Create</a>
+        <a href="/admin/user/create" class="btn btn-primary btn-sm float-right ml-3">Create</a>
     </div>
     <div class="card-body">
         <table class='table table-striped' id="table1">
